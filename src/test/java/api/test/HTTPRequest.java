58adbc008ba2f809544f980927de25f1f8167dc1
Shoumik Rouf <lethirteenthchild@gmail.com>
2023-03-31 16:03:21 +0600

    Crud testing done

--- src/test/java/api/test/HTTPRequest.java
+++ src/test/java/api/test/HTTPRequest.java
@@ -1,13 +1,19 @@
 package api.test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
+import java.util.*;
+
 
 public class HTTPRequest {
 
-	@Test
-void getUser() {
+	int id;
+	
+	@Test (priority=1)
+	void getUsers() {
 		given()
 		
 		.when()
@@ -16,5 +22,55 @@ void getUser() {
 			.statusCode(200)
 			.body("page",equalTo(2))
 			.log().all();
+    }
+	
+	@Test (priority=2)
+	void createUser() {
+		
+		HashMap myMap = new HashMap();
+		myMap.put("name","Sun");
+		myMap.put("job","energySupply");
+		
+	    id=given()
+			.contentType("application/json")
+			.body(myMap)
+		.when()
+			.post("https://reqres.in/api/users")
+			.jsonPath().getInt("id");
+			
+
 	}
+
+
+	@Test(priority=3, dependsOnMethods= {"createUser"})	
+	void updateUser() {
+		
+		HashMap myMap = new HashMap();
+		myMap.put("name","Moon");
+		myMap.put("job","beautySupply");
+	
+		  given()
+			.contentType("application/json")
+			.body(myMap)
+			
+		  .when()
+		  	.put("https://reqres.in/api/users/"+id)
+		  	
+		
+	      .then()
+	      	.statusCode(200);
+	      	
+}
+	@Test(priority=4)
+	void deleteUser() {
+		given()
+		
+		.when()
+			.delete("https://reqres.in/api/users/"+id)
+		.then()
+		    .statusCode(204)
+      	    .log() .all();
+	}
+	
+	
 }
